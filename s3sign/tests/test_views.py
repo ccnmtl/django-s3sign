49dff746d5f372b9f8f2707e1ceb73d78cf62e1c
Anders Pearson <anders@columbia.edu>
2016-03-23 13:32:04 +0000

    couple more tests

--- s3sign/tests/test_views.py
+++ s3sign/tests/test_views.py
@@ -9,3 +9,11 @@ class TestView(TestCase):
     def test_now(self):
         v = SignS3View()
         self.assertIsNotNone(v.now())
+
+    def test_now_time(self):
+        v = SignS3View()
+        self.assertIsNotNone(v.now_time())
+
+    def test_new_uuid(self):
+        v = SignS3View()
+        self.assertIsNotNone(v.new_uuid())
