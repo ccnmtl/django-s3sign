7b6e316808e62ded331956f625dc976459912814
Nikolas Nyby <nikolas@gnu.org>
2021-03-08 13:47:56 -0500

    Fix setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-import os
+import io
+from os import path
 from setuptools import setup
 
-ROOT = os.path.abspath(os.path.dirname(__file__))
+
+this_directory = path.abspath(path.dirname(__file__))
+long_description = ''
+with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="django-s3sign",
@@ -10,13 +16,14 @@ setup(
     author_email="ctl-dev@columbia.edu",
     url="https://github.com/ccnmtl/django-s3sign",
     description="Django view for AWS S3 signing",
-    long_description=open(os.path.join(ROOT, 'README.md')).read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['Django>=1.11', 'nose'],
     scripts=[],
     license="GPL3",
     platforms=["any"],
     zip_safe=False,
-    package_data = {'': ['*.*']},
+    package_data={'': ['*.*']},
     packages=['s3sign'],
     test_suite='nose.collector',
     include_package_data=True,
